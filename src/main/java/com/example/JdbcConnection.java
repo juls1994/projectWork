14766bde775d0a9d9b7680e91c6c01260856e76b
juls1994 <116438098+juls1994@users.noreply.github.com>
2022-10-23 22:51:16 +0200

    jdbc connection

--- src/main/java/com/example/JdbcConnection.java
+++ src/main/java/com/example/JdbcConnection.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
 import java.sql.SQLException;
- 
+
 /**
  * This program demonstrates how to establish database connection to Microsoft
  * SQL Server.
@@ -13,15 +13,13 @@ import java.sql.SQLException;
  */
 public class JdbcConnection {
     public static void main(String[] args) {
- 
+
         Connection conn = null;
 
         try {
- 
-            String dbURL = "jdbc:sqlserver://localhost\\sqlexpress";
-            String user = "ZG\\CONGIL";
-            String pass = "";
-            conn = DriverManager.getConnection(dbURL, user, pass);
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            String dbURL = "jdbc:sqlserver://localhost;databaseName=project_work;user=giulia;password=GiuliaGiulia;encrypt=False;trustservercertificate=False;security=True;pooling=False;multipleactiveresultsets=False;";
+            conn = DriverManager.getConnection(dbURL);
             if (conn != null) {
                 DatabaseMetaData dm = (DatabaseMetaData) conn.getMetaData();
                 System.out.println("Driver name: " + dm.getDriverName());
@@ -29,8 +27,8 @@ public class JdbcConnection {
                 System.out.println("Product name: " + dm.getDatabaseProductName());
                 System.out.println("Product version: " + dm.getDatabaseProductVersion());
             }
- 
-        } catch (SQLException ex) {
+
+        } catch (SQLException | ClassNotFoundException ex) {
             ex.printStackTrace();
         } finally {
             try {
